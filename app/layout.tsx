6dbe5161db79a3c12bdebc69b9a90a5a1007a794
felixtsu <humaeks@gmail.com>
2025-11-16 12:01:47 +0800

    chore(analytics): hardcode GA ID G-WGE0B4BNDR and remove env var

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,27 +16,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
-
   return (
     <html lang="zh-HK">
       <body className={`${inter.className} antialiased bg-white text-[#1E1E1E]`}>
-        {gaMeasurementId ? (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
-              strategy="afterInteractive"
-            />
-            <Script id="gtag-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaMeasurementId}');
-              `}
-            </Script>
-          </>
-        ) : null}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-WGE0B4BNDR`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WGE0B4BNDR');
+          `}
+        </Script>
         <Navigation />
         <main className="relative mx-auto flex min-h-[calc(100vh-5rem)] w-full flex-col">
           {children}
